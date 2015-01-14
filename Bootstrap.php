73edae38336154e06ee2f8010094bba5530c0baa
Pavel <pavel.agalecky@gmail.com>
2015-01-14 13:25:18 +0300

    Added bootstrap

---
+++ Bootstrap.php
@@ -0,0 +1,26 @@
+<?php
+
+namespace yz\admin\mailer;
+
+use yii\base\Application;
+use yii\base\BootstrapInterface;
+
+
+/**
+ * Class Bootstrap
+ */
+class Bootstrap implements BootstrapInterface
+{
+    /**
+     * Bootstrap method to be called during application bootstrap stage.
+     * @param Application $app the application currently running
+     */
+    public function bootstrap($app)
+    {
+        $app->i18n->translations['admin/mailer'] = [
+            'class' => 'yii\i18n\PhpMessageSource',
+            'basePath' => '@yz/admin/mailer/common/messages',
+            'sourceLanguage' => 'en-US',
+        ];
+    }
+}
